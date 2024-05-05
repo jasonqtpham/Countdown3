7eb74e1b36678909b07240c56a7211513d53c480
Jason Pham <deh5yb@virginia.edu>
2024-05-05 13:10:21 -0400

    Basic weather info fetch

--- WeatherApp/src/components/Weather.jsx
+++ WeatherApp/src/components/Weather.jsx
@@ -7,19 +7,29 @@ import Button from '@mui/material/Button';
 const Weather = () => {
   const [location, setLocation] = useState("");
   const [weatherData, setWeatherData] = useState(null);
-  const [coordinate, setCoordinate] = useState(null);
+  const [coordinate, setCoordinate] = useState({latitude: 0, ongitude: 0});
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     console.log('Submitted:', location);
-    const geoData = await fetchData(location);
-    console.log('name ',geoData.name);
-    setCoordinate({lat: geoData.lat, lon: geoData.lon});
+    const geoData = await fetchGeoData(location);
+    console.log('name geo data',geoData.name);
+    setCoordinate({latitude: geoData.lat, longitude: geoData.lon});
     
     setLocation('');
   };
 
   useEffect(() => {console.log(coordinate)}, [coordinate]);
+
+  useEffect(() => {
+    const fetchWeatherData = async () => {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${coordinate.latitude}&lon=${coordinate.longitude}&units=imperial&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
+        const json = await response.json();
+        setWeatherData(json);
+        console.log('this is in fetch weather data:', json);
+    };
+    fetchWeatherData()
+  },[coordinate]);
 
   return (
     <LocationInput location={location} setLocation={setLocation} handleSubmit={handleSubmit}/>
@@ -42,10 +52,13 @@ const LocationInput = ({ location, setLocation, handleSubmit}) => {
     );
 };
 
-const fetchData = async (location) => {
+const fetchGeoData = async (location) => {
     const response = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&limit=1&appid=${import.meta.env.VITE_WEATHER_API_KEY}`);
     const json = await response.json();
-    console.log(json[0])
+    console.log('This is in fetchGeoData', json[0])
     return json[0];
 }
+
+
+
 export default Weather;
